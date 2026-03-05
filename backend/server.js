1380bb07bac13c3913d2b8ab73e5d1dbc5ecf50c
Victor Paul Mallavalli <victormallavalli7@gmail.com>
2026-03-05 12:07:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://unrivaled-haupia-fd6d95.netlify.app/",
   credentials: true
 }));
 console.log("OPENAI_API_KEY:", process.env.OPENAI_API_KEY);
